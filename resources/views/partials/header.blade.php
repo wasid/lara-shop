88b13d3884290596084845c87a5158a3995a6897
Wasid Hossain <wasidmail@gmail.com>
2017-10-17 18:17:48 +0000

    added juery for dropdown options

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,17 @@
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart</a></li>
-      <li><a href="{{ route('user.signup') }}"><span class="glyphicon glyphicon-log-in"></span> Sign Up</a></li>
+      <!--<li><a href="{{ route('user.signup') }}"><span class="glyphicon glyphicon-log-in"></span> Sign Up</a>-->
+      <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+              Name <span class="caret"></span>
+          </a>
+          <ul class="dropdown-menu" role="menu">
+              <li><a href="#"><i class="fa fa-btn fa-pencil-square-o"></i>Profile</a></li>
+              <li><a href="#"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+          </ul>
+      </li>      
+      </li>
     </ul>
   </div>
 </nav>
