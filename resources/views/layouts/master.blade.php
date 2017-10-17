57227223edc14d56b544638fbb18dfe539974555
Wasid Hossain <wasidmail@gmail.com>
2017-10-17 09:34:48 +0000

    Product Index View added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,10 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" />
-    @yield('style')
+    <!--<link rel="stylesheet" href="{{ URL::to('src/css/custom.css') }}" type="text/css" />-->
+    <link rel="stylesheet" href="{!! asset('css/custom.css') !!}" type="text/css" />
+    <!--@yield('style')-->
 </head>
 <body>
-    @yield('content')
+    @include('partials.header')
+    <div class="container">
+        @yield('content')    
+    </div>
+    
+    
     <script
       src="https://code.jquery.com/jquery-1.12.4.min.js"
       integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,9 +1,21 @@
 @extends('layouts.master')
 
 @section('title')
-Lara Shop
+Lara Shopgit
 @endsection
 
 @section('content')
-<h1>It's Working!</h1>
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail cartimg">
+      <img src="http://universe.byu.edu/wp-content/uploads/2015/01/HP4cover.jpg" alt="..." class="img-reponsive">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <h4>$112</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo quos quisquam ab optio est incidunt recusandae fugit, alias doloremque dicta.</p>
+        <p><a href="#" class="btn btn-primary" role="button">Add to Cart</a></p>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
