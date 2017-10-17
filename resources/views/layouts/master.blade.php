c8950b6235b2d15bbbfdb3c6ff8efa6741d3bbd1
Wasid Hossain <wasidmail@gmail.com>
2017-10-17 08:18:37 +0000

    first commit for lara-shop

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" />
+    @yield('style')
+</head>
+<body>
+    @yield('content')
+    <script
+      src="https://code.jquery.com/jquery-1.12.4.min.js"
+      integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+      crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/css" />
+    @yield('script')
+</body>
+</html>
---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title')
+Lara Shop
+@endsection
+
+@section('content')
+<h1>It's Working!</h1>
+@endsection
