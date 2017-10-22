9877fb803f135b94fdc434c2ed7553cbc0a6be35
Wasid Hossain <wasidmail@gmail.com>
2017-10-22 10:58:08 +0000

    added hamburger icon to the header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,25 +1,34 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="/">Lara Shop</a>
+        <a class="navbar-brand" href="/">Lara Shop</a>
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
     </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="/">Home</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart</a></li>
-      <li><a href="{{ route('user.signup') }}"><span class="glyphicon glyphicon glyphicon-check"></span> Sign Up</a></li>
-      <!--<li><a href="{{ route('user.signup') }}"><span class="glyphicon glyphicon-log-in"></span> Sign Up</a>-->
-      <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-              Name <span class="caret"></span>
-          </a>
-          <ul class="dropdown-menu" role="menu">
-              <li><a href="#"><i class="fa fa-btn fa-pencil-square-o"></i>Profile</a></li>
-              <li><a href="#"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-          </ul>
-      </li>      
-      </li>
-    </ul>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="/">Home</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart</a></li>
+        <li><a href="{{ route('user.signup') }}"><span class="glyphicon glyphicon glyphicon-check"></span> Sign Up</a></li>
+        <!--<li><a href="{{ route('user.signup') }}"><span class="glyphicon glyphicon-log-in"></span> Sign Up</a>-->
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                Name <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu" role="menu">
+                <li><a href="#"><i class="fa fa-btn fa-pencil-square-o"></i>Profile</a></li>
+                <li><a href="#"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+            </ul>
+        </li>      
+        </li>
+      </ul>
+    </div>
+
   </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -10,49 +10,54 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Get all products
 Route::get('/', [
     'uses' => 'ProductController@getIndex',
     'as'=> 'shop.index'
     ]);
+
+// Signup 
 Route::get('/signup', [
     'uses' => 'UserController@getSignup',
     'as'=> 'user.signup'
     ]);
+    
 Route::post('/signup', [
     'uses' => 'UserController@postSignup',
     'as'=> 'user.signup'
     ]);
 
+// add products to the cart
 Route::get('/add/{id}', [
     'uses' => 'CartController@addCart',
     'as'=> 'cart.add'
     ]);
 
+// show all cart products
 Route::get('/cart/show', [
     'uses' => 'CartController@showCart',
     'as'=> 'cart.show'
     ]);
     
-
+// add quantity of one product in cart page
 Route::get('/cart/addqt/{id}', [
     'uses' => 'CartController@addQtCart',
     'as'=> 'cart.addqt'
     ]);
     
-
+// decrease quantity of one product in cart page
 Route::get('/cart/remove/{id}', [
     'uses' => 'CartController@removeCart',
     'as'=> 'cart.remove'
     ]);
     
-
+// remove a product from cart page
 Route::get('/cart/removethis/{id}', [
     'uses' => 'CartController@removethisCart',
     'as'=> 'cart.removethis'
     ]);
     
-
+// remove all items from cart
 Route::get('/cart/removeall/', [
     'uses' => 'CartController@removeallCart',
     'as'=> 'cart.removeall'
